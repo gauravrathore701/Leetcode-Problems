4e6666ad97310b9d5139f9d570f2e66d3cc01ff7
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-03-13 23:29:41 +0530

    Update (28)FirstOccurrenceInaString.py

--- (28)FirstOccurrenceInaString.py
+++ (28)FirstOccurrenceInaString.py
@@ -2,18 +2,9 @@ word = 'leetcode'
 n = 'leeto'
 
 def strStr(haystack, needle):
-    if len(needle) > len(haystack):
-        return -1
-    l, h = 0, 0
-    while h < len(haystack):
-        if haystack[h] != needle[l]:
-            h = h-l+1
-            l = 0
-        if haystack[h] == needle[l]:
-            h += 1
-            l += 1
-            if l == len(needle):
-                return h-l
+    for i in range(len(haystack) +1 -len(needle)):
+        if haystack[i: i+len(needle)] == needle:
+            return i
     return -1
 
 print(strStr(word,n))
