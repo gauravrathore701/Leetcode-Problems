e715fa12df8d728ff056ef636e88bbc863b60152
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-03-26 19:34:21 +0530

    Update (350)IntersectionOfArrays2.py

--- (350)IntersectionOfArrays2.py
+++ (350)IntersectionOfArrays2.py
@@ -1,14 +1,14 @@
-import Counter from collections
+from collections import Counter
 
-class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        c = Counter(nums1)
-        output = []
-        for n in nums2:
-            if c[n]>0:
-                output.append(n)
-                c[n]-=1
-        return output
+
+def intersect(nums1, nums2):
+    c = Counter(nums1)
+    output = []
+    for n in nums2:
+        if c[n]>0:
+            output.append(n)
+            c[n]-=1
+    return output
 
 
 n1 = [1,2,3,4]
