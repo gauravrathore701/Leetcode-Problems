e35875ac7712bb20807a36ad3ed94a47d6a242c5
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-04-02 12:39:21 +0530

    Create (15)3Sum.py

---
+++ (15)3Sum.py
@@ -0,0 +1,24 @@
+n = [-1,0,1,0]
+
+def threeSum(nums):
+    res = []
+    nums.sort()
+
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
+        l, r = i + 1 , len(nums) - 1
+        while l < r:
+            sum = a + nums[l] + nums[r]
+            if sum > 0:
+                r -= 1
+            elif sum < 0:
+                l += 1
+            else:
+                res.append([nums[a], nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l-1] and l<r:
+                    l += 1
+    return res
+
+print(threeSum(n))
