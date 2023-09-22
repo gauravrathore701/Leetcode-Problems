120250cfb0ad63888db2c7f15bc1d744412b9d78
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-09-22 20:26:34 +0530

    Update (347)KTopFrequent.py

--- (347)KTopFrequent.py
+++ (347)KTopFrequent.py
@@ -1,13 +1,16 @@
 def topKFrequent(nums, k):
-    a = {}
-    f = [[] for i in range(len(nums)+1)]
-    for i in range(len(nums)):
-        a[nums[i]] = 1 + a.get(nums[i], 0)
-    for n, c in a.items():
-        f[c].append(n)
+    count = {}     # defining the HashMap
+    freq = [[] for i in range(len(nums)+1)] # same number of empty array as len of nums to count each repition
+    # loop to count the number of times every value has occured
+    for n in nums:
+        count[n] = 1 + count.get(n, 0)
+    # going through each value we counted
+    for n, c in count.items(): #count.items() return key: value pairs that's been added to the dict
+        freq[c].append(n)
     res = []
-    for i in range(len(f) - 1,0,-1):
-        for n in f[i]:
+    # going through the frequency array in reverse order to return the number of k most frequent elements
+    for i in range(len(freq) - 1,0,-1):
+        for n in freq[i]:
             res.append(n)
             if len(res) == k:
                 return res
