3bc8aa3d0ad9a282bf488144505853c4f45996af
Gaurav Rathore <63956933+thatsleepytalker@users.noreply.github.com>
2023-09-28 23:46:07 +0530

    Update (1)TwoSum.py

--- (1)TwoSum.py
+++ (1)TwoSum.py
@@ -1,6 +1,7 @@
 def twoSum(nums, target):
+    # Initializing the Hash Map in python also known as Dictionary
     m = {}
-
+    # loop to count 
     for i, n in enumerate(nums):
         diff = target - n
         if diff in m:
